4bb3079aa07bc7bb10890b4d354a67a5482a7807
thequanic <85480632+thequanic@users.noreply.github.com>
2022-12-26 19:56:32 +0530

    added idle condition

--- Ques4_FCFS.c
+++ Ques4_FCFS.c
@@ -40,28 +40,30 @@ int main()
         }
     }
 
+    int time=array[0].arrival_time;
+    int idle_time=0;
+
     for(i=0;i<n;i++)
     {
-        //calculating completion time, awaiting_time and turn around time for each process
-        if(i==0)
+        if(array[i].arrival_time>time)
         {
-            //for first process waiting time will be zero //comp_time=arrival+brust //turn around=comp-arrival
-            array[i].comp_time=array[i].arrival_time+array[i].brust_time;
-            array[i].waiting_time=0;
-            array[i].turnaround_time=array[i].comp_time-array[i].arrival_time;
+            idle_time++;
+            time++;
+            i--;
+            continue;
+        }
 
-        }
-        else
-        {
-            //comp=comp of previous process + brust // turnaround= comp-arrival //waiting=turnaround-brust
-            array[i].comp_time=array[i-1].comp_time+array[i].brust_time;
-            array[i].turnaround_time=array[i].comp_time-array[i].arrival_time;
-            array[i].waiting_time=array[i].turnaround_time-array[i].brust_time;
-        }
+        array[i].comp_time=time+array[i].brust_time;
+        array[i].turnaround_time=array[i].comp_time-array[i].arrival_time;
+        array[i].waiting_time=array[i].turnaround_time-array[i].brust_time;
+
+        time+=array[i].brust_time;
     }
 
     //printing results
     float averg_wait_time=0,averg_turn_time=0;
+    float cpu_utilization=((time-idle_time)/(float)time)*100;
+
     for(i=0;i<n;i++)
     {
         printf("\nPid arrival_time brust_time waiting_time completion_time turnaround_time\n");
@@ -71,6 +73,9 @@ int main()
     }
 
     printf("\nAverage Waiting Time=%f, Average turn around time=%f",averg_wait_time/n,averg_turn_time/n);
+    printf("\nTotal time taken by cpu to complete all processes: %d",time);
+    printf("\nTotal idle time of cpu to complete all processes: %d",idle_time);
+    printf("\nCPU UTILIZATION: %f percent",cpu_utilization);
 
 
 }
