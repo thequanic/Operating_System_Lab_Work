27052ab3682ba0e8308d871755bbb63b8bed637b
thequanic <85480632+thequanic@users.noreply.github.com>
2022-12-26 23:27:41 +0530

    commit

--- Ques6_round_robin_without_queue.c
+++ Ques6_round_robin_without_queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+typedef struct process
 {
     int pid;
     int arrival_time;
@@ -91,8 +92,28 @@ int main()
         {
             temp=0;
         }
-
-
     }
+
+     for(i=0;i<n;i++)
+    {
+        array[i].turnaround_time=array[i].comp_time-array[i].arrival_time;
+        array[i].waiting_time=array[i].turnaround_time-array[i].brust_time;
+    }
+
+    //printing results
+    float averg_wait_time=0,averg_turn_time=0;
+    float cpu_utilization=((time-idle_time)/(float)time)*100;
+    for(i=0;i<n;i++)
+    {
+        printf("\nPid arrival_time brust_time waiting_time completion_time turnaround_time\n");
+        printf("%3d %12d %10d %12d %15d %15d\n",array[i].pid,array[i].arrival_time,array[i].brust_time,array[i].waiting_time,array[i].comp_time,array[i].turnaround_time);
+        averg_wait_time+=array[i].waiting_time;
+        averg_turn_time+=array[i].turnaround_time;
+    }
+
+    printf("\nAverage Waiting Time=%f, Average turn around time=%f",averg_wait_time/n,averg_turn_time/n);
+    printf("\nTotal time taken by cpu to complete all processes: %d",time);
+    printf("\nTotal idle time of cpu to complete all processes: %d",idle_time);
+    printf("\nCPU UTILIZATION: %f percent",cpu_utilization);
 
 }
